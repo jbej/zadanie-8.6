c4acf6037ec0cf1de73136fec96165cf60bef16b
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2017-11-29 22:06:07 +0200

    Add scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,12 @@
-var number = a;
-var number = b;
-wynik =(a * a) - (2 * a * b) + (b * b);
+var a = 3;
+var b = 1;
+alert((a * a) - (2 * a * b) + (b * b));
 
-var value = "4";
-
-if (value > 0) {
-    console.log('Liczba jest większa od 0');    
-} else if (value < 0) {
-    console.log('Liczba jest mniejsza od 0');
-} else {
-    console.log('Liczba równa się 0');
+var value = 4;
+if (value >= 0) {
+    alert('It is number positive!');
+}  else if (value <= 0) {
+    alert('It is number negative!');
+}  else {
+    alert('Unknown value!');
 }
-
-alert("Liczba jest większa od 0");
-
-// secend part
-
-var number = a;
-var number = b;
-wynik =(a * a) - (2 * a * b) + (b * b);
-
-var value = "4";
-
-var value = a>b && a<b ? 'negative' : 'positive';
-console.log(value);
-alert("positive");
