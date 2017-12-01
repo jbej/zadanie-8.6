1b3073acacd02bcf62362db66ee0954a0d84dd1e
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2017-12-01 21:05:09 +0200

    Add scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,16 @@ var a = 3;
 var b = 1;
 alert((a * a) - (2 * a * b) + (b * b));
 
-var value = 4;
-if (value >= 0) {
-    alert('It is number positive!');
-}  else if (value <= 0) {
-    alert('It is number negative!');
-}  else {
-    alert('Unknown value!');
+var value = a+b;
+if (value > 0) {
+    console.log('It is number positive!');
+}  else if (value < 0) {
+    console.log('It is number negative!');
+}  
+
+var value = a+b;
+if (value == 0) {
+    console.log('Number is 0');
+} else {
+    console.log('Number unknown')
 }
