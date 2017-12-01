51c714352031d8ab048369929f955411585fec55
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2017-12-01 22:03:45 +0200

    Add scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-var a = 3;
-var b = 1;
-alert((a * a) - (2 * a * b) + (b * b));
+var a = prompt("podaj liczbe");
+var b = prompt("podaj liczbe");
+var value = (a * a) - (2 * a * b) + (b * b);
 
-var value = a+b;
 if (value > 0) {
-    console.log('It is number positive!');
+    console.log('It is number positive!', value);
 }  else if (value < 0) {
-    console.log('It is number negative!');
-}  
-
-var value = a+b;
-if (value == 0) {
-    console.log('Number is 0');
+    console.log('It is number negative!', value);
+}  else if (value == 0) {
+    console.log('Number is 0', value);
 } else {
-    console.log('Number unknown')
+    console.log('Number unknown', value);
 }
